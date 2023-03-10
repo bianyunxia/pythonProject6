a8cd972250f9c4922db862fbc092ea5af263604d
bianer <15932642912@163.com>
2023-03-10 20:36:07 +0800

    Initial commit

---
+++ test_case1/test_data1_class.py
@@ -0,0 +1,5 @@
+class Test_data1:
+    def test_001(self):
+        print("this test_001")
+    def test_002(self):
+        print("this test_002")
---
+++ test_case1/test_data2_clss.py
@@ -0,0 +1,5 @@
+class Test_data2:
+    def test_003(self):
+        print("this test_003")
+    def test_004(self):
+        print("this test_004")
